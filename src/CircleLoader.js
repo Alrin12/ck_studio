bce4e2a1cc8d216d33f39496e3b0c6eebf881d36
Ck-Republic <Alrin12@github.com>
2018-10-20 18:12:11 +0900

    init

---
+++ src/CircleLoader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CircleLoader extends Component {
+  render() {
+    return (
+      <div className={'LoaderContainer'}>
+        <div className={'Circle'}/>
+      </div>
+    );
+  }
+}
+
+const styles = {
+
+}
